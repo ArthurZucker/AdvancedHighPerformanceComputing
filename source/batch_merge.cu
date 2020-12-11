a263237b8ad2c2019c91300977ca46c8e6868b22
arthurzucker <arthur.zucker@gmail.com>
2020-12-11 16:38:05 +0100

    automatic push

--- source/batch_merge.cu
+++ source/batch_merge.cu
@@ -9,6 +9,57 @@
 #include <time.h>
 #include "batch_merge.h"
 
+
+__global__ void mergeSmallBatch_k(int *__restrict__ all_M,int *M,int *all_sA, int *all_sB,int d){
+    int tidx = threadIdx.x%d;
+    int Qt = (threadIdx.x-tidx)/d;
+    int gbx = Qt + blockIdx.x*(blockDim.x/d);
+    // printf("threadIdx = %d; blockIdx = %d; tidx = %d, Qt = %d, gbx = %d, combined=%d\n",threadIdx.x,blockIdx.x,tidx,Qt,gbx,tidx+gbx*d);
+
+    int sA = all_sA[gbx];
+    int sB = all_sB[gbx];
+    int *A = &all_M[gbx*d];
+    int *B = &all_M[gbx*d+sA];
+   
+    int i = blockDim.x*blockIdx.x + threadIdx.x;
+    if(tidx<d){
+        int2 K;
+        int2 P;
+        if(tidx>sA){
+            K = {tidx-sA,sA};
+            P = {sA,tidx-sA};
+        }
+        else{
+            K = {0,tidx};
+            P = {tidx,0};
+        }
+        while(1){
+            int offset = int(abs(K.y-P.y)/2);
+            int2 Q = {K.x+offset,K.y-offset};
+
+            if(Q.y >= 0 && Q.x <= sB && (Q.y == sA || Q.x == 0 || A[Q.y] > B[Q.x-1])){
+                if(Q.x==sB || Q.y==0 || A[Q.y-1]<=B[Q.x]){
+                   if(Q.y < sA && (Q.x == sB || A[Q.y]<=B[Q.x])){
+                        M[i] = A[Q.y];
+                        // printf("index globale = %d, threadIdx = %d; blockIdx = %d; tidx = %d, Qt = %d, gbx = %d, __ldg(&A[%d]) = %d\n",i,threadIdx.x,blockIdx.x,tidx,Qt,gbx,Q.y,__ldg(&A[Q.y]));
+                   }
+                   else{
+                        M[i] = B[Q.x];
+                        // printf("index globale = %d, threadIdx = %d; blockIdx = %d; tidx = %d, Qt = %d, gbx = %d, __ldg(&B[%d]) = %d\n",i,threadIdx.x,blockIdx.x,tidx,Qt,gbx,Q.x,__ldg(&B[Q.x]));
+                   }
+                   break;
+                }
+                else{
+                   K = {Q.x+1,Q.y-1};
+                }
+            }
+            else{
+                P = {Q.x-1,Q.y+1};
+            }
+        }
+    }
+}
+
 __global__ void mergeSmallBatch_k_ldg(int *__restrict__ all_M,int *M,int *all_sA, int *all_sB,int d){
     int tidx = threadIdx.x%d;
     int Qt = (threadIdx.x-tidx)/d;
--- source/batch_merge.h
+++ source/batch_merge.h
@@ -4,4 +4,5 @@
 __global__ void mergeSmallBatch_k_ldg(int *__restrict__ all_M,int *M,int *all_sA, int *all_sB,int d);
 __global__ void mergeSmallBatch_k(int *__restrict__ all_M,int *M,int *all_sA, int *all_sB,int d);
 __global__ void SortSmallBatch_k( int *__restrict__ all_M,int *M,int i,int d);
+__global__ void SortSmallBatch_k_ldg(int *__restrict__ all_M,int *M,int si,int d);
 #endif
