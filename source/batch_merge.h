459389c24c3c030586a9e4153a9a9b604eed4516
arthurzucker <arthur.zucker@gmail.com>
2020-12-12 15:29:55 +0100

    automatic push

--- source/batch_merge.h
+++ source/batch_merge.h
@@ -1,6 +1,5 @@
 #ifndef BATCH_MERGE_h
 #define BATCH_MERGE_h
-
 __global__ void mergeSmallBatch_k_ldg(int *__restrict__ all_M,int *M,int *all_sA, int *all_sB,int d);
 __global__ void mergeSmallBatch_k(int *__restrict__ all_M,int *M,int *all_sA, int *all_sB,int d);
 __global__ void SortSmallBatch_k( int *__restrict__ all_M,int *M,int i,int d);
