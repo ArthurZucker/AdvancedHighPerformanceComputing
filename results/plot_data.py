6e16594193ffec0edef5ce7983928e25a98b9447
ArthurZucker <arthur.zucker@gmail.com>
2020-12-13 11:43:28 +0100

    automatic push

--- results/plot_data.py
+++ results/plot_data.py
@@ -3,6 +3,32 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy import stats
+sns.set_style('darkgrid')
+
+# ______________________Question 1_____________________________
+
+df = pd.read_csv("results1.csv",usecols=["type","memory", "time"])
+palette = sns.color_palette("rocket_r")
+sns.barplot(
+    data=df,
+    x="time", y="type",
+    hue="memory",palette=palette,
+    height=5, aspect=.75
+)
+plt.show()
+
+
+# ______________________Question 2_____________________________
+
+df = pd.read_csv("results2.csv",usecols=["Kernel","type","memory", "time"])
+palette = sns.color_palette("rocket_r")
+sns.barplot(
+    data=df,
+    x="time", y="type",palette=palette,
+    height=5, aspect=.75, facet_kws=dict(sharex=False),
+)
+plt.show()
+
 
 
 # ______________________Question 3_____________________________
@@ -10,12 +36,39 @@ df = pd.read_csv("results3.csv",usecols=["d", "time"])
 print(df)
 sns.relplot(x="d", y="time", kind="line", data=df)
 plt.show()
+plt.savefig("question3.svg", format="svg")
+
+# ______________________Question 1_____________________________
+
+df = pd.read_csv("results4.csv",usecols=["type","memory", "time"])
+palette = sns.color_palette("rocket_r")
+sns.barplot(
+    data=df,
+    x="time", y="type",
+    hue="memory",palette=palette,
+    height=5, aspect=.75
+)
+plt.show()
+
 
 # ______________________Question 5_____________________________
-df = pd.read_csv("results5.csv",usecols=["d", "time"])
+df = pd.read_csv("results5.csv",usecols=["N","d", "time"])
 # plt.figure(1)
 # plot = 521
 N=10
+
+palette = sns.color_palette("rocket_r")
+
+# Plot the lines on two facets
+sns.relplot(
+    data=df,
+    x="d", y="time",
+    hue="N",
+    kind="line", palette=palette,
+    height=5, aspect=.75, facet_kws=dict(sharex=False),
+)
+plt.show()
+
 for i in range(6):
     temp_df = df[i*10:(i+1)*10]
     print(temp_df)
@@ -26,3 +79,4 @@ for i in range(6):
     N*=10
 
 plt.show()
+
